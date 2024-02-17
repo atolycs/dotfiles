590c247dcd39132a42b8357cc1123bddadf6bf5b
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-02-17 19:06:04 +0900

    Update telescope

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,11 +2,31 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-
 local keymap = vim.keymap
 local opts = { noremap = true, silent = false }
-
 
 -- exapnd, contract
 keymap.set("n", "+", "<C-a>")
 keymap.set("n", "-", "<C-x>")
+
+-- new Tab
+-- keymap.set("n", "te", ":tabedit")
+-- keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+-- keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+
+-- buffer tab change
+keymap.set("n", "<tab>", ":BufferLineCycleNext<Return>", opts)
+keymap.set("n", "<S-tab>", ":BufferLineCyclePrev<Return>", opts)
+keymap.set("n", "q", ":bdelete<Return>", opts)
+
+-- move window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+
+-- select All
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Diagnostics
+keymap.set("n", "<C-j>", function()
+	vim.diagnostic.goto_next()
+end, opts)
