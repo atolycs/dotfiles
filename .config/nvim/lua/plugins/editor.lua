70d86bb3f9484d8ddc7c047427a7afedc917db4f
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-02-19 08:52:48 +0900

    Fix Open Telescope File Browser

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,82 +1,82 @@
 return {
-	{
-		"telescope.nvim",
-		dependencies = {
-			"nvim-telescope/telescope-file-browser.nvim",
-		},
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-file-browser.nvim",
+    },
 
-		keys = {
-			{
-				"sf",
-				function()
-					local telescope = require("telescope")
+    keys = {
+      {
+        "sf",
+        function()
+          local telescope = require("telescope")
 
-					local function telescope_buffer_dir()
-						return vim.fn.expand("%:p:h")
-					end
+          local function telescope_buffer_dir()
+            return vim.fn.expand("%:p:h")
+          end
 
-					telescope.extensions.file_browser.file_browser({
-						path = "%:p:h",
-						cwd = telescope_buffer_dir(),
-						hidden = true,
-						initial_mode = "normal",
-						layout_config = { height = 40 },
-					})
-				end,
-				desc = "Open File Browser with the path of the current buffer",
-			},
-		},
+          telescope.extensions.file_browser.file_browser({
+            path = "%:p:h",
+            cwd = telescope_buffer_dir(),
+            hidden = true,
+            initial_mode = "normal",
+            layout_config = { height = 40 },
+          })
+        end,
+        desc = "Open File Browser with the path of the current buffer",
+      },
+    },
 
-		config = function(_, opts)
-			local telescope = require("telescope")
-			local actions = require("telescope.actions")
-			local fb_actions = require("telescope").extensions.file_browser.actions
+    config = function(_, opts)
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+      local fb_actions = require("telescope").extensions.file_browser.actions
 
-			opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
-				layout_strategy = "horizontal",
-				layout_config = { prompt_position = "top" },
-				wrap_results = true,
-				sorting_strategy = "ascending",
-				winblend = 0,
-				mappings = {
-					n = {},
-				},
-			})
+      opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        wrap_results = true,
+        sorting_strategy = "ascending",
+        winblend = 0,
+        mappings = {
+          n = {},
+        },
+      })
 
-			opts.pickers = {
-				diagnostics = {
-					initial_mode = "normal",
-				},
-			}
+      opts.pickers = {
+        diagnostics = {
+          initial_mode = "normal",
+        },
+      }
 
-			opts.extensions = {
-				file_browser = {
-					hijack_netrw = true,
-					mappings = {
-						-- insert mode mapping
-						["n"] = {
-							["N"] = fb_actions.create,
-							["h"] = fb_actions.goto_parent_dir,
-							--[""]
-							["/"] = function()
-								vim.cmd("startinsert")
-							end,
-							["<C-u>"] = function(prompt_bufnr)
-								for i = 1, 10 do
-									actions.move_selection_previous(prompt_bufnr)
-								end
-							end,
-							["<C-d>"] = function(prompt_bufnr)
-								for i = 1, 10 do
-									actions.move_selection_next(prompt_bufnr)
-								end
-							end,
-						},
-					},
-				},
-			}
-			telescope.setup(opts)
-			require("telescope").load_extension("file_browser")
-		end,
-	},
+      opts.extensions = {
+        file_browser = {
+          hijack_netrw = true,
+          mappings = {
+            -- insert mode mapping
+            ["n"] = {
+              ["N"] = fb_actions.create,
+              ["h"] = fb_actions.goto_parent_dir,
+              --[""]
+              ["/"] = function()
+                vim.cmd("startinsert")
+              end,
+              ["<C-u>"] = function(prompt_bufnr)
+                for i = 1, 10 do
+                  actions.move_selection_previous(prompt_bufnr)
+                end
+              end,
+              ["<C-d>"] = function(prompt_bufnr)
+                for i = 1, 10 do
+                  actions.move_selection_next(prompt_bufnr)
+                end
+              end,
+            },
+          },
+        },
+      }
+      telescope.setup(opts)
+      require("telescope").load_extension("file_browser")
+    end,
+  },
 }
--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,32 +1,38 @@
 return {
-	{
-		"projekt0n/github-nvim-theme",
-	},
-	{
-		"nvimdev/dashboard-nvim",
-		opts = function(_, opts)
-			opts.config.center = {
-				{ action = "ene | startinsert", desc = "New File", icon = " ", key = "n" },
-				{ action = "Telescope file_browser", desc = "Open File", icon = " ", key = "o" },
-				{
-					action = [[lua require("lazyvim.util").telescope.config_files()()]],
-					desc = "Config",
-					icon = "󰖷 ",
-					key = "c",
-				},
-				{ action = "Lazy", desc = "Lazy plugin manager", icon = "󰒲 ", key = "l" },
-				{ action = "qa", desc = "Quit", icon = " ", key = "q" },
-			}
+  {
+    "projekt0n/github-nvim-theme",
+  },
+  {
+    "nvimdev/dashboard-nvim",
+    opts = function(_, opts)
+      opts.config.center = {
+        { action = "ene | startinsert", desc = "New File", icon = " ", key = "n" },
+        { action = "Telescope file_browser hidden=true initial_mode=normal", desc = "Open File", icon = " ", key = "o" },
+        {
+          action = [[lua require("lazyvim.util").telescope.config_files()()]],
+          desc = "Config",
+          icon = "󰖷 ",
+          key = "c",
+        },
+        { action = "Lazy", desc = "Lazy plugin manager", icon = "󰒲 ", key = "l" },
+        { action = "qa", desc = "Quit", icon = " ", key = "q" },
+      }
 
-			if vim.o.filetype == "lazy" then
-				vim.cmd.close()
-				vim.api.nvim_create_autocmd("User", {
-					pattern = "DashboardLoaded",
-					callback = function()
-						require("lazy").show()
-					end,
-				})
-			end
-		end,
-	},
+      if vim.o.filetype == "lazy" then
+        vim.cmd.close()
+        vim.api.nvim_create_autocmd("User", {
+          pattern = "DashboardLoaded",
+          callback = function()
+            require("lazy").show()
+          end,
+        })
+      end
+    end,
+  },
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 3000,
+    }
+  }
 }
