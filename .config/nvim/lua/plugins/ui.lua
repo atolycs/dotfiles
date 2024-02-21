0f878f2b3421bec06b85e2e40bbff7865846b6ad
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-02-21 09:41:51 +0900

    NeoVim customize

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,38 +1,42 @@
 return {
-  {
-    "projekt0n/github-nvim-theme",
-  },
-  {
-    "nvimdev/dashboard-nvim",
-    opts = function(_, opts)
-      opts.config.center = {
-        { action = "ene | startinsert", desc = "New File", icon = " ", key = "n" },
-        { action = "Telescope file_browser hidden=true initial_mode=normal", desc = "Open File", icon = " ", key = "o" },
-        {
-          action = [[lua require("lazyvim.util").telescope.config_files()()]],
-          desc = "Config",
-          icon = "󰖷 ",
-          key = "c",
-        },
-        { action = "Lazy", desc = "Lazy plugin manager", icon = "󰒲 ", key = "l" },
-        { action = "qa", desc = "Quit", icon = " ", key = "q" },
-      }
-
-      if vim.o.filetype == "lazy" then
-        vim.cmd.close()
-        vim.api.nvim_create_autocmd("User", {
-          pattern = "DashboardLoaded",
-          callback = function()
-            require("lazy").show()
-          end,
-        })
-      end
-    end,
-  },
-  {
-    "rcarriga/nvim-notify",
-    opts = {
-      timeout = 3000,
-    }
-  }
+	{
+		"projekt0n/github-nvim-theme",
+	},
+	{
+		"nvimdev/dashboard-nvim",
+		opts = function(_, opts)
+			opts.config.center = {
+				{ action = "ene | startinsert", desc = "New File", icon = " ", key = "n" },
+				{
+					action = "Telescope file_browser hidden=true initial_mode=normal",
+					desc = "Open File",
+					icon = " ",
+					key = "o",
+				},
+				{
+					action = [[lua require("lazyvim.util").telescope.config_files()()]],
+					desc = "Config",
+					icon = "󰖷 ",
+					key = "c",
+				},
+				{ action = "Lazy", desc = "Lazy plugin manager", icon = "󰒲 ", key = "l" },
+				{ action = "qa", desc = "Quit", icon = " ", key = "q" },
+			}
+			if vim.o.filetype == "lazy" then
+				vim.cmd.close()
+				vim.api.nvim_create_autocmd("User", {
+					pattern = "DashboardLoaded",
+					callback = function()
+						require("lazy").show()
+					end,
+				})
+			end
+		end,
+	},
+	{
+		"rcarriga/nvim-notify",
+		opts = {
+			timeout = 3000,
+		},
+	},
 }
