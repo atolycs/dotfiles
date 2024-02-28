f8dd6859c2b77234aeefb7fe9d5ee0b7a930d45f
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-02-28 17:42:54 +0900

    Update nvim Back to editor

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,4 +1,21 @@
 return {
+
+	-- {
+	-- 	"akinsho/bufferline.nvim",
+	-- 	event = "VeryLazy",
+	-- 	keys = {
+	-- 		{ "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next Tab" },
+	-- 		{ "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev Tab" },
+	-- 	},
+	-- 	opts = {
+	-- 		options = {
+	-- 			mode = "tabs",
+	-- 			show_buffer_close_icons = false,
+	-- 			show_close_icon = false,
+	-- 		},
+	-- 	},
+	-- },
+
 	{
 		"projekt0n/github-nvim-theme",
 	},
@@ -21,7 +38,8 @@ return {
 				},
 				{ action = "Lazy", desc = "Lazy plugin manager", icon = "󰒲 ", key = "l" },
 				{ action = "Mason", desc = "Mason LSP Manager", icon = " ", key = "m" },
-				{ action = "checkhealth", desc = "Check NeoVim Health", icon = " " },
+				{ action = "checkhealth", desc = "Check NeoVim Health", icon = " ", key = "h" },
+				{ action = "bdelete", desc = "Back to Editor", icon = "󱇨 ", key = "b" },
 				{ action = "qa", desc = "Quit", icon = "󰩈 ", key = "q" },
 			}
 			if vim.o.filetype == "lazy" then
