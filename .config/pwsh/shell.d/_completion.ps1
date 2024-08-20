10294d0b01425436aae888874378b7d6835848ff
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-08-20 11:09:29 +0900

    feat: add oh-my-posh completion

--- .config/pwsh/shell.d/_completion.ps1
+++ .config/pwsh/shell.d/_completion.ps1
@@ -1,1 +1,15 @@
-Invoke-Expression -Command $(gh completion -s powershell | Out-String)
+if (Get-Command gh) {
+  Invoke-Expression -Command $(gh completion -s powershell | Out-String)
+}
+
+if (Get-Command spotify_player.exe) {
+    Invoke-Expression -Command $(spotify_player.exe generate powershell | Out-String)
+}
+
+if (Get-Command go-spotify-cli-windows-amd64.exe) {
+    Invoke-Expression -Command $(go-spotify-cli-windows-amd64.exe completion powershell | Out-String)
+}
+
+if (Get-Command oh-my-posh.exe) {
+    Invoke-Expression -Command $(oh-my-posh.exe completion powershell | Out-String)
+}
