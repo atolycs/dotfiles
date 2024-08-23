46332b3d452a951e157bfdf7dd7350a936b8ad9e
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-08-23 12:55:57 +0900

    feat(pwsh): completion add

--- .config/pwsh/shell.d/_completion.ps1
+++ .config/pwsh/shell.d/_completion.ps1
@@ -13,3 +13,15 @@ if (Get-Command go-spotify-cli-windows-amd64.exe) {
 if (Get-Command oh-my-posh.exe) {
     Invoke-Expression -Command $(oh-my-posh.exe completion powershell | Out-String)
 }
+
+# Winget completion
+
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+        $Local:word = $wordToComplete.Replace('"', '""')
+        $Local:ast = $commandAst.ToString().Replace('"', '""')
+        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
