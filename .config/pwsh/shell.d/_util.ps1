16095b4f30373e0261f933d1b9e072ed5e640d33
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-08-20 14:43:55 +0900

    feat: update refreshenv

--- .config/pwsh/shell.d/_util.ps1
+++ .config/pwsh/shell.d/_util.ps1
@@ -2,7 +2,7 @@
 # Unix like which command
 function which() {
   Param(
-    $command
+    [System.String]$command
   )
 
   Get-Command -Name $command -ErrorAction SilentlyContinue | 
@@ -38,7 +38,54 @@ function touch() {
 # refresh PATH Environment
 
 function refreshenv() {
-    $env:PATH = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+    
+    Write-Output "Reloading PATH Environment..."
+
+    # Thank you GitHub Copilot !
+
+    # Github copilot generate start
+    
+    # set Sepalate
+    $sep = ";"
+    
+    # Get System PATH Environment
+    $systemPath = [System.Environment]::GetEnvironmentVariable("Path", "Machine")
+    
+    # Get User PATH Environment
+    $userPath = [System.Environment]::GetEnvironmentVariable("Path", "User")
+
+    # Get Process PATH Environment
+    $processPath = [System.Environment]::GetEnvironmentVariable("Path", "Process")
+
+    # Join System User PATH Environment and unique to array
+    $basePathArr = @()
+    if ($systemPath) { $basePathArr += $systemPath -split [regex]::Escape($sep) }
+    if ($userPath) { $basePathArr += $userPath -split [regex]::Escape($sep) }
+
+    $basePathArr = $basePathArr | Where-Object { $_ -ne "" } | Select-Object -unique
+
+    # Process PATH Environment to array
+    $processPathArr = @()
+    if ($processPath) { $processPathArr += $processPath -split [regex]::Escape($sep) }
+    $processPathArr = $processPathArr | Where-Object { $_ -ne "" }
+
+    # Get Added PATH
+    $addedPaths = $processPathArr | Where-Object { $_ -and ( $_ -notin $basePathArr ) } | Select-Object -Unique
+
+    # Sync PATH
+    $newPathArr = @()
+    foreach ($p in ($basePathArr + $addedPaths)) {
+      if ( $p -and ($newPathArr -notcontains $p) ) { $newPathArr += $p }
+    }
+    $newPath = $newPathArr -join $sep
+
+    # Set Process Environment PATH
+    [System.Environment]::SetEnvironmentVariable("Path", $newPath, "Process")
+
+    # Github copilot generate end
+
+
+    #$env:PATH = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
 }
 
 function realpath() {
