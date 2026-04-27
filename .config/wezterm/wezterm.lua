c93d26f731aae9ed6c2cb06bfca4c901617779c0
Atolycs <111569596+atolycs@users.noreply.github.com>
2026-04-27 19:08:54 +0900

    feat: add wezterm wezterm and keybindins

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,9 +6,20 @@ config.initial_rows = 30
 config.initial_cols = 120
 config.use_ime = true
 
+-- font size
+config.font_size = 12.0
+
 config.window_background_opacity = 0.7
 config.macos_window_background_blur = 20
 
 config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+
+config.window_frame = {
+	inactive_titlebar_bg = "none",
+	active_titlebar_bg = "none",
+}
+
+config.show_new_tab_button_in_tab_bar = false
 
 return config
